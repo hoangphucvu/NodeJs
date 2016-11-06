1f87cc9163bec8f144e69fca5bfd116c34a6afcd
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-11-06 16:48:46 +0700

    Seperate route

--- API/app.js
+++ API/app.js
@@ -12,42 +12,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 var port = process.env.PORT || 3000;
 
-
-var bookRouter = express.Router();
-
-bookRouter.route('/Books')
-    .post(function(req,res){
-        var book = new Book(req.body);
-        book.save();
-        res.status(201).send(book);
-    })
-    .get(function(req,res){
-
-        var query = {};
-
-        if(req.query.genre)
-        {
-            query.genre = req.query.genre;
-        }
-        Book.find(query, function(err,books){
-            if(err)
-                res.status(500).send(err);
-            else
-                res.json(books);
-        });
-    });
-
-bookRouter.route('/Books/:bookId')
-    .get(function(req,res){
-
-
-        Book.findById(req.params.bookId, function(err,book){
-            if(err)
-                res.status(500).send(err);
-            else
-                res.json(book);
-        });
-    });
+//() to excute function
+bookRouter = require('./routes/BookRoute')(Book);
 
 app.use('/api', bookRouter);
 
