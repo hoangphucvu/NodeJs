19c9d70f463cc335b521a363ed79f38032637e80
hoangphucvu <ngohungphuc95@gmail.com>
2016-11-08 07:59:28 +0700

    Middleware

--- API/app.js
+++ API/app.js
@@ -8,7 +8,9 @@ var db = mongoose.connect('mongodb://localhost/bookAPI');
 var Book = require('./models/bookModel');
 
 var app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 var port = process.env.PORT || 3000;
 
@@ -19,10 +21,10 @@ app.use('/api', bookRouter);
 
 
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.send('welcome to my API!');
 });
 
-app.listen(port, function(){
+app.listen(port, function() {
     console.log('Gulp is running my app on  PORT: ' + port);
 });
--- API/routes/bookRoute.js
+++ API/routes/bookRoute.js
@@ -1,58 +1,62 @@
 /*
-* @Author: Ngo Hung Phuc
-* @Date:   2016-11-06 16:44:08
-* @Last Modified by:   hoangphucvu
-* @Last Modified time: 2016-11-07 15:05:05
-*/
+ * @Author: Ngo Hung Phuc
+ * @Date:   2016-11-06 16:44:08
+ * @Last Modified by:   hoangphucvu
+ * @Last Modified time: 2016-11-08 07:57:31
+ */
 var express = require('express');
-var routes = function(Book){
-	var bookRouter = express.Router();
+var routes = function(Book) {
+    var bookRouter = express.Router();
 
-	bookRouter.route('/Books')
-	.post(function(req,res){
-		var book = new Book(req.body);
-		book.save();
-		res.status(201).send(book);
-	})
-	.get(function(req,res){
+    bookRouter.route('/Books')
+        .post(function(req, res) {
+            var book = new Book(req.body);
+            book.save();
+            res.status(201).send(book);
+        })
+        .get(function(req, res) {
 
-		var query = {};
+            var query = {};
 
-		if(req.query.genre)
-		{
-			query.genre = req.query.genre;
-		}
-		Book.find(query, function(err,books){
-			if(err)
-				res.status(500).send(err);
-			else
-				res.json(books);
-		});
-	});
+            if (req.query.genre) {
+                query.genre = req.query.genre;
+            }
+            Book.find(query, function(err, books) {
+                if (err)
+                    res.status(500).send(err);
+                else
+                    res.json(books);
+            });
+        });
 
-	bookRouter.route('/Books/:bookId')
-	.get(function(req,res){
-		Book.findById(req.params.bookId, function(err,book){
-			if(err)
-				res.status(500).send(err);
-			else
-				res.json(book);
-		});
-	})
-	.put(function(req,res){
-		Book.findById(req.params.bookId, function(err,book){
-			if(err)
-				res.status(500).send(err);
-			else
-				book.title = req.body.title;
-			book.author = req.body.author;
-			book.genre = req.body.genre;
-			book.read = req.body.read;
-			book.save();
-			res.json(book);
-		});
-	});
-	return bookRouter;
+    //declear middleware
+    bookRouter.use('/:bookId', function(req, res, next) {
+        Book.findById(req.params.bookId, function(err, book) {
+            if (err)
+                res.status(500).send(err);
+            else if (book) {
+                //add to request
+                req.book = book;
+                next();
+            } else {
+                res.status(404).send('no book exists');
+            }
+        });
+    });
+
+    bookRouter.route('/Books/:bookId')
+        .get(function(req, res) {
+            res.json(req.book);
+        })
+        .put(function(req, res) {
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            req.book.save();
+            req.json(req.book);
+        });
+    return bookRouter;
 };
 
 module.exports = routes;
