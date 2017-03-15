cd200703660fa322a459f01701049307c15f246c
hoangphucvu <ngohungphuc95@gmail.com>
2017-03-15 14:21:02 +0700

    Get post info

--- Node Blog System/nodeblog/routes/index.js
+++ Node Blog System/nodeblog/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var mongo = require('mongodb');
+var db = require('monk')('mongodb://tony:070695@ds131480.mlab.com:31480/nodeblog');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var db = req.db;
+    var posts = db.get('posts');
+    posts.find({}, {}, function(err, posts) {
+        res.render('index', {"posts": posts});
+    });
 });
-
 module.exports = router;
