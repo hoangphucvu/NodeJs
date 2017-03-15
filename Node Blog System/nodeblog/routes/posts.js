2f535b38d7ec44434ead931fb798705db2ec06a3
hoangphucvu <ngohungphuc95@gmail.com>
2017-03-15 15:31:51 +0700

    Todo fix multer upload image

--- Node Blog System/nodeblog/routes/posts.js
+++ Node Blog System/nodeblog/routes/posts.js
@@ -3,7 +3,13 @@ var router = express.Router();
 var mongo = require('mongodb');
 var db = require('monk')('mongodb://tony:070695@ds131480.mlab.com:31480/nodeblog');
 router.get('/add', function(req, res, next) {
-    res.render('addpost', {"title": "Add Post"});
+    var categories = db.get('categories');
+    categories.find({}, {}, function(req, categoriesList) {
+        res.render('addpost', {
+            "title": "Add Post",
+            "categories": categoriesList
+        });
+    });
 });
 router.post('/add', function(req, res, next) {
     var title = req.body.title;
@@ -12,6 +18,7 @@ router.post('/add', function(req, res, next) {
     var author = req.body.author;
     var date = new Date();
     if (req.files.mainimage) {
+        console.log(req.files.mainimage);
         var mainImageOriginalName = req.files.mainimage.originalname;
         var mainImageName = req.files.mainimage.name;
         var mainImageMime = req.files.mainimage.mimetype;
@@ -20,6 +27,35 @@ router.post('/add', function(req, res, next) {
         var mainImageSize = req.files.mainimage.size;
     } else {
         var mainImageName = 'noimage.png';
+    }
+    //form validation
+    req.checkBody('title', 'Title is required').notEmpty();
+    req.checkBody('body', 'Body is required').notEmpty();
+    var errors = req.validationErrors();
+    if (errors) {
+        res.render('addpost', {
+            'errors': errors,
+            'title': title,
+            'body': body
+        });
+    } else {
+        var posts = db.get('posts');
+        posts.insert({
+            'title': title,
+            'body': body,
+            'category': category,
+            'date': date,
+            'author': author,
+            'mainimage': mainimage
+        }, function(err, post) {
+            if (err) {
+                res.send('Error');
+            } else {
+                req.flash('success', 'Post submitted');
+                res.location('/');
+                res.redirect('/');
+            }
+        });
     }
 });
 module.exports = router;
